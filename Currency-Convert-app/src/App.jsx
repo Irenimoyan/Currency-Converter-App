f36dc607dc089259d708ec235ca3cf3ed300de24
Irenimoyan <sheutomalli@gmail.com>
2025-09-02 12:50:40 +0100

    fix the navbbar error

--- Currency-Convert-app/src/App.jsx
+++ Currency-Convert-app/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./pages/Register";
